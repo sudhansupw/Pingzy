0fbb3450801686a529427f3a8b0157aa60bb5cce
sudhansupw <sudhansudashsonu543@gmail.com>
2026-02-08 20:31:08 +0530

    first commit

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://pingzy-1.onrender.com/api",
   withCredentials: true,
 });
--- Frontend/src/lib/socket.js
+++ Frontend/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = "https://pingzy-1.onrender.com"
 
 const socket = io(BASE_URL, {
   autoConnect: false,
